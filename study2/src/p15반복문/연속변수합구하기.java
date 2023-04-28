2314281ecd386d3304e328dcec21bcd12824551e
Class01 <Class01@DESKTOP-UR65MFL>
2023-04-28 16:31:24 +0900

    04.28

---
+++ study2/src/p15반복문/연속변수합구하기.java
@@ -0,0 +1,15 @@
+package p15반복문;
+
+public class 연속변수합구하기 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		
+		for(int i=1;i<=10;i++) {
+			sum += i;
+		}
+		
+		System.out.println("sum = " + sum);
+		
+	}
+}
